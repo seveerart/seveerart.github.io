ec934ad3dab7ecde84a70042d9a7f06876fc6bc2
seveerart <72038150+seveerart@users.noreply.github.com>
2021-08-27 18:10:49 -0700

    ughhhhhhhh

---
+++ script.js
@@ -0,0 +1,9 @@
+// this is me scripty shit beech
+
+// $('.mobile-menu-btn').on('click', function() {
+// 	if($(this).hasClass('active')) {
+// 		$('.mobile-menu-btn').removeClass('active');
+// 	} else {
+// 		$('.mobile-menu-btn').addClass('active');
+// 	}
+// });
